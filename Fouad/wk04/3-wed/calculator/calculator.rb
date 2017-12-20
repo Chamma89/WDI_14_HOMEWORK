1288872110436d55c1dab09c3537f24b5a3be08f
fouad <Chamma.fouad@gmail.com>
2017-12-20 23:59:49 +1100

    in class hashes task and ruby calculator

---
+++ Fouad/wk04/3-wed/calculator/calculator.rb
@@ -0,0 +1,73 @@
+require 'pry'
+
+
+
+
+loop do
+	puts 'please choose from the following'
+
+	puts 'addition, subtraction, multiplication, division, exponent, square root'
+
+	calculator_function = gets.chomp
+
+		puts 'select first number'
+
+		a = gets.to_i
+
+		puts 'select second number'
+
+		b = gets.to_i
+
+		if calculator_function == "addition"
+			
+		# add
+		print "Your result is " 
+		puts a + b
+			
+
+		elsif calculator_function == "subtraction"	
+
+			# subtract
+			print "Your result is "
+			puts  a - b
+				
+		elsif calculator_function == "multiplication"
+				
+			# multiply
+			print "Your result is "
+			puts  a * b
+				
+		elsif calculator_function == "division"
+
+			# division	
+			print "Your result is "			
+			puts  (a / b)
+				
+		elsif calculator_function == "exponent"
+			puts a ** b
+
+		elsif calculator_function == "square root"
+			puts Math.sqrt(a + b)		
+		end	
+
+		# to ask question again. Loop
+
+		puts 'Would you like to try again? yes or no'
+
+		ask_again = gets.chomp
+
+
+		if ask_again == "yes"
+
+		calculator_function = true
+
+
+		elsif ask_again == "no"
+	
+		puts "You quit!"
+
+		break
+
+		end
+
+end
